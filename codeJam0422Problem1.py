6c26a431639ca3f7aec323f27b401b935de5e1fc
K. Rorabaugh <102931841+k-roraba@users.noreply.github.com>
2022-05-31 09:37:36 -0500

    Update codeJam0422Problem1.py

--- codeJam0422Problem1.py
+++ codeJam0422Problem1.py
@@ -2,16 +2,27 @@
 # punched cards problem
 # https://codingcompetitions.withgoogle.com/codejam/round/0000000000876ff1/0000000000a4621b
 
-# produced runtime error
+# original produced runtime error, cause unknown.   
+# changing the output array to store every solution (rather than every line) as a value 
+# got it to pass.  Maybe in the old method I was using up too much memory.
+
+
+# the analysis section recommends nested for loops, but it uses a modulo check to decide which char to 
+# print at which position.  Same time complexity, but I wonder which operation 
+# (checking position, or running through the loop my way) is more efficient.
+# definitely didn't think of checking position.
 
 case = int(input())
 output = []
 
 #get parameters, create output
 for i in range(case):
+    output.append("Case #")
+    output[i] = output[i] + str(i+1) + ":\n"
     r, c = [int(x) for x in input().split()]
+
     #row 0
-    output.append("..+")
+    output[i] += "..+"
     for j in range(c-1):
         output[i] += "-+"
     
@@ -42,6 +53,4 @@ for i in range(case):
     
 #print output
 for i in range(case):
-    print("Case #{}:".format(i+1))
     print(output[i])
-    
