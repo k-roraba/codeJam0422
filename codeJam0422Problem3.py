df3dcbe1e069140ba58307a62b10792809c48b3d
K. Rorabaugh <102931841+k-roraba@users.noreply.github.com>
2022-05-31 10:42:06 -0500

    Update codeJam0422Problem3.py

--- codeJam0422Problem3.py
+++ codeJam0422Problem3.py
@@ -2,58 +2,44 @@
 #  dice rolls
 #  https://codingcompetitions.withgoogle.com/codejam/round/0000000000876ff1/0000000000a46471
 
-# this worked for the first test set, but exceeded the time limit on the second
-# I think I should sort and fiddle with the dice during intake
-# instead of having one pass for intake, another for sorting, and another for actually analysing...
+# this is a leaner version; it passes test set 1, but gets the wrong answer on test set 2...?
 
-# will come back to it
-
-def bubblesort(elements):
-# Looping from size of array from last index[-1] to index [0]
-    for n in range(len(elements)-1, 0, -1):
-        for i in range(n):
-            if elements[i] > elements[i + 1]:
-	            #swapping data if the element is less than next element in the array
-                elements[i], elements[i + 1] = elements[i + 1], elements[i]
-
-def removeBad(elements, counter, start):
-    if counter >= len(elements):
-        return
-    for n in range(start, len(elements)):
-        if(elements[n] <= n):
-            elements.remove(elements[n])
-            start = n
-            break
-
-    removeBad(elements, counter+1, start)
-
+# incidentally, the Google solution has something closer to my original solution:
+# one pass to sort, then a linear loop to determine the length straight.
+# They were more insightful with their counting method, though.
                      
 case = int(input())
 output = []
 
 for i in range(case):
-    output.append(0)
+    output.append("Case #")
+
     n = int(input())
     dice = []
     vals = [int(x) for x in input().split()]
-    for j in range(n):
-        dice.append(vals[j])
 
-    bubblesort(dice)
-    removeBad(dice, 0, 0)
+    dice.append(vals[0])
+    ans = 1
 
-    for j in range(len(dice)):
-        if(dice[j] > j):
-            output[i] += 1
-        else:
-            break
+    for j in range(n - 1):
+        for k in range(len(dice)):
+            if vals[j+1] < dice[k]:
+                dice.insert(k, vals[j+1])
+                if dice[k] > k+1:
+                    ans += 1
+                break
+        
+        if vals[j+1] >= dice[len(dice)-1]:
+            dice.append(vals[j+1])
+            if dice[k] > k+1:
+                ans += 1
 
-
-            
+    output[i] = output[i] + str(i+1) + ": " + str(ans)
         
 for i in range(case):
-    print("Case #{}:".format(i+1), output[i])
+    print(output[i])
 
-#this worked!!!
         
+
+
 
